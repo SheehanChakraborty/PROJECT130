ae93fefd4c7a92abecd1a0e78356d61216350b86
SheehanChakraborty <77329538+SheehanChakraborty@users.noreply.github.com>
2022-05-28 21:11:46 +0530

    Add files via upload

--- project130.py
+++ project130.py
@@ -1,15 +1,11 @@
 import pandas as pd 
 import csv
 
-df = pd.read_csv("main.csv")
+df = pd.read_csv("PROJECT128.csv")
 print(df.columns)
 print(df.shape)
-del df ["Luminosity"]
-del df ["Star_name.1"]
-del df ["Distance.1"]
-del df ["Mass.1"]
-del df ["Radius.1"]
-del df ["Unnamed: 0"]
-del df ["Unnamed: 6"]
+del df ["Star_name"]
+del df ["Distance"]
+del df ["Mass"]
 print(df.columns)
 df.to_csv("c130.csv")
